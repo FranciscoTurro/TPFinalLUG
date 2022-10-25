c6aad86a4db9152a686c61f4a32c43793e3fd15d
FranciscoTurro <francisco.gtrro@outlook.com.ar>
2022-10-25 19:47:00 -0300

    a

--- controllers/cart.ts
+++ controllers/cart.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
-import { CartModel } from "../models/cart";
+import {CartModel}  from "../models/cart";
 import { Model } from "mongoose";
 
-export const cartController = {};
+export const cartController = {
+    post: async (request:Request, response:Response) => {
+        try {
+            if(CartModel.find()) {
+                console.log("No puede haber mas de un carrito existente")
+                return
+            }
+            const body = request.body
+            const model = new CartModel({...body})
+        } catch (error) {
+            response.status(500).send("Error")
+        }
+    }
+};
